5572a4c79c483846d31c4cbf2f3abdc752e75b68
Peter Georg <peter.georg@physik.uni-regensburg.de>
2016-12-12 13:58:37 +0100

    rename id -> nodeid

--- src/providers/scif/nodeid.cpp
+++ src/providers/scif/nodeid.cpp
@@ -12,7 +12,7 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-#include "id.hpp"
+#include "nodeid.hpp"
 #include <stdexcept>
 extern "C"
 {
