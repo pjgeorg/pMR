ab6171a320841fb57fa7bd78ec88de4d7e9c9fff
Peter Georg <peter.georg@physik.uni-regensburg.de>
2017-06-03 18:05:00 +0200

    Fix compiler options for pmr-config

--- src/tool/config/config.cmake
+++ src/tool/config/config.cmake
@@ -1,4 +1,11 @@
 cmake_minimum_required(VERSION 2.8.11)
+
+if("${CMAKE_CXX_FLAGS}" STREQUAL "")
+    set(CMAKE_CXX_FLAGS "-Wall -Werror -O3 -std=c++11")
+else()
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Werror -O3 -std=c++11")
+endif()
+
 add_executable(config
     info.cpp
     parameter.cpp
