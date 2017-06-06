e3022da0027313f947b0778188fad1a863fd7dbf
Daniel Richtmann <daniel.richtmann@gmail.com>
2017-06-06 12:33:27 +0200

    Fix installation directory of config binary

--- src/tool/config/install.cmake
+++ src/tool/config/install.cmake
@@ -1,4 +1,4 @@
-get_filename_component(INSTALL_PREFIX ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_INSTALL_PREFIX} ABSOLUTE)
+get_filename_component(INSTALL_PREFIX ${CMAKE_INSTALL_PREFIX} ABSOLUTE)
 execute_process(COMMAND cmake . -DCMAKE_INSTALL_PREFIX:PATH=${INSTALL_PREFIX} WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/src/tool/config/config)
 execute_process(COMMAND make WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/src/tool/config/config)
 execute_process(COMMAND make install WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/src/tool/config/config)
